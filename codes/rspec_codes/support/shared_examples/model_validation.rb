a5dc65e998fa355f2d031e21bebe32da57ead750
msickpaler <kenyadayoyoyon@gmail.com>
2020-10-04 17:20:53 +0900

    add model_validation

--- codes/rspec_codes/support/shared_examples/model_validation.rb
+++ codes/rspec_codes/support/shared_examples/model_validation.rb
@@ -12,3 +12,16 @@ RSpec.shared_examples "validation error" do |column, message|
     expect(subject.errors[column]).to eq(message)
   end
 end
+
+RSpec.shared_examples "has no association" do |target|
+  it "has no association to #{target}" do
+    expect(described_class.reflect_on_association(target)).to be_nil
+  end
+end
+
+# association is like :belongs_to, :has_many
+RSpec.shared_examples "has association" do |association, target|
+  it "#{association} #{target}" do
+    expect(described_class.reflect_on_association(target).macro).to eq association
+  end
+end
